cb7d35fa5b433c3b223b000bca3436b68b8aaef7
justinrasler <85715506+justinrasler@users.noreply.github.com>
2022-06-15 17:40:10 -0400

    google maps needs work

--- bnb-frontend/pages/index.js
+++ bnb-frontend/pages/index.js
@@ -1,15 +1,45 @@
-import { sanityClient } from '../sanity'
+import { sanityClient, urlFor } from "../sanity"
+import Link from "next/link"
+import { isMultiple } from "../utils"
+import DashboardMap from "../components/DashboardMap"
 
-const Home = (properties) => {
+const Home = ({ properties }) => {
   console.log(properties)
   return (
     <>
+      {properties && (
+        <div className="main">
+          <div className="feed-container">
+            <h1>Places to stay near you</h1>
+            <div className="feed">
+              {properties.map((property) => (
+                <Link href={`property/${property.slug.current}`}>
+                  <div key={property._id} className="card">
+                    <img src={urlFor(property.mainImage)} />
+                    <p>
+                      {property.reviews.length} review
+                      {isMultiple(property.reviews.length)}
+                    </p>
+                    <h3>{property.title}</h3>
+                    <h3>
+                      <b>£{property.pricePerNight}/per Night</b>
+                    </h3>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="map">
+            <DashboardMap properties={properties} />
+          </div>
+        </div>
+      )}
     </>
   )
 }
 
 export const getServerSideProps = async () => {
-  const query = '*[_type == "property"]'
+  const query = '*[ _type == "property"]'
   const properties = await sanityClient.fetch(query)
 
   if (!properties.length) {
@@ -21,8 +51,8 @@ export const getServerSideProps = async () => {
   } else {
     return {
       props: {
-        properties
-      }
+        properties,
+      },
     }
   }
 }
